86a206bc24b7394a9ee7eb94c9e55d2a47fcc1b2
LinShakka <victor.fdemeira@gmail.com>
2023-01-26 13:20:06 -0300

    blog CRUD renamed

--- blog/main.py
+++ blog/main.py
@@ -17,7 +17,7 @@ def get_db():
         db.close()
 
 @app.post("/blog", status_code = status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
+def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title = request.title, body = request.body)
     db.add(new_blog)
     db.commit()
@@ -25,7 +25,7 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 @app.delete("/blog/{id}", status_code = status.HTTP_204_NO_CONTENT)
-def erase(id, db: Session = Depends(get_db)):
+def erase_blog(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
@@ -35,7 +35,7 @@ def erase(id, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/blog/{id}", status_code = status.HTTP_202_ACCEPTED)
-def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+def update_blog(id, request: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
@@ -45,12 +45,12 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     return "The blog was updated successfully"
 
 @app.get("/blog", response_model = List[schemas.ShowBlog])
-def all(db: Session = Depends(get_db)):
+def all_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 @app.get("/blog/{id}", status_code = status.HTTP_200_OK, response_model = schemas.ShowBlog)
-def show(id, response: Response, db: Session = Depends(get_db)):
+def show_blog(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
@@ -58,3 +58,4 @@ def show(id, response: Response, db: Session = Depends(get_db)):
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return {"detail": f"Blog with the id {id} is not available"}
     return blog
+
